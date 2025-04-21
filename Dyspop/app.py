844da38b03a9fa201a820cd6acb22f19dd00db50
isiah-a <isiaha478@gmail.com>
2025-04-21 18:34:50 -0400

    Made changes to models, might not need some setters?

--- Dyspop/app.py
+++ Dyspop/app.py
@@ -18,22 +18,7 @@ app.config['FLASK_DEBUG'] = 1
 if __name__ == '__main__':
     socketio.run(app, port=5555, debug = True)
 
-class Dyspo:
-    def __init__(self, id, username, email, password, mood_list, user_id, timestamp, mood_name, mood_rating, notes):
-        self.id = id
-        self.username = username
-        self.email = email
-        self.password = password
-        self.mood_list = mood_list
-        self.user_id = user_id
-        self.timestamp = timestamp
-        self.mood_name = mood_name
-        self.mood_rating = mood_rating
-        self.notes = notes
 
-    @property
-    def get_id(self):
-        return self.id
 
 
 
--- Dyspop/routes.py
+++ Dyspop/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, request, redirect, url_for, flash, session
 from flask_login import current_user, login_user, logout_user
-from flask_login.utils import login_required
+from flask_login import current_user, login_required
 import datetime
+from .models import db, MoodEntry, User
 import markdown
 from flask import Blueprint, render_template, request, redirect, url_for
 
